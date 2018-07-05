cc1b574ca777e55bbd8a13967c03e9b25b955bba
koyakoya90 <klickscience@gmail.com>
2018-07-05 14:02:13 +0700

    first 4

--- get.php
+++ get.php
@@ -1,10 +1,21 @@
 <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
 <script type="text/javascript">
 $( document ).ready(function() {
-    
+
 });
 $.get("http://smk-maarifkudus.sch.id/tiny.php?url=https://www.facebook.com/idmahbub", function(shorturl){
         alert(shorturl)
     });
 </script>
+<a href="http://facebook.com">fb</a>
+
+<script src='https://cdn.rawgit.com/koyakoya90/meong/master/meong.js' type='text/javascript'></script>
+<script type="text/javascript"><!--
+protected_links = "blogspot,instagram,blogger,facebook,google";
+auto_anonymize();
+//--></script>
+<script type="text/javascript">
+protected_links="blogspot,instagram,blogger,facebook,google";
+auto_kanjutkan();</script>
+
 <div id="aa"></div>
